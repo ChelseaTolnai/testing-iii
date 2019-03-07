ed73d8fa6a13c64c441529a2ae1054bac1e2e17c
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-03-07 15:46:35 -0500

    Added tests for control button functions with display panel outputs.

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { render } from 'react-testing-library';
+import { render , fireEvent} from 'react-testing-library';
 import 'jest-dom/extend-expect';
 import renderer from 'react-test-renderer';
 
@@ -21,10 +21,55 @@ describe('<Dashboard />', () => {
         const { container } = render(<Dashboard />);
         
         expect(container.children).toHaveLength(2);
-
         expect(container.children[0]).toHaveClass('display panel');
         expect(container.children[1]).toHaveClass('controls panel');
+    });
 
-	});
+    it('confirms control buttons function properly with expected output', () => {
+        const { getByText } = render(<Dashboard />);
 
+        // Default
+        expect(getByText(/Unlocked/)).toBeInTheDocument();
+        expect(getByText(/Open$/)).toBeInTheDocument();
+        expect(getByText(/Lock Gate/)).toBeInTheDocument();
+        expect(getByText(/Lock Gate/)).toHaveAttribute('disabled'); 
+        expect(getByText(/Close Gate/)).toBeInTheDocument();
+
+        fireEvent.click(getByText(/Close Gate/));
+
+        // After Close Gate clicked
+        expect(getByText(/Unlocked/)).toBeInTheDocument();
+        expect(getByText(/Closed/)).toBeInTheDocument(); 
+        expect(getByText(/Lock Gate/)).toBeInTheDocument();
+        expect(getByText(/Lock Gate/)).not.toHaveAttribute('disabled'); 
+        expect(getByText(/Open Gate/)).toBeInTheDocument();
+
+        fireEvent.click(getByText(/Lock Gate/));
+        
+        // After Lock Gate clicked
+        expect(getByText(/Locked/)).toBeInTheDocument();
+        expect(getByText(/Closed/)).toBeInTheDocument(); 
+        expect(getByText(/Unlock Gate/)).toBeInTheDocument();
+        expect(getByText(/Open Gate/)).toBeInTheDocument();
+        expect(getByText(/Open Gate/)).toHaveAttribute('disabled'); 
+
+        fireEvent.click(getByText(/Unlock Gate/));
+        
+        // After Unlock Gate clicked
+        expect(getByText(/Unlocked/)).toBeInTheDocument();
+        expect(getByText(/Closed/)).toBeInTheDocument(); 
+        expect(getByText(/Lock Gate/)).toBeInTheDocument();
+        expect(getByText(/Lock Gate/)).not.toHaveAttribute('disabled'); 
+        expect(getByText(/Open Gate/)).toBeInTheDocument();
+
+        fireEvent.click(getByText(/Open Gate/));
+        
+        // After Open Gate clicked === Reverts back to Default
+        expect(getByText(/Unlocked/)).toBeInTheDocument();
+        expect(getByText(/Open$/)).toBeInTheDocument();
+        expect(getByText(/Lock Gate/)).toBeInTheDocument();
+        expect(getByText(/Lock Gate/)).toHaveAttribute('disabled'); 
+        expect(getByText(/Close Gate/)).toBeInTheDocument();
+    });
+    
 })
